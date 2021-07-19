51e790bea2ad37dcdc2e7a767d1c333361c8c795
Shun Cheung <scheung@qmo.io>
2021-07-18 23:44:04 -0400

    feat(Player): remove unused import

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,14 +1,5 @@
 import { Field, ObjectType } from "type-graphql";
-import { TypeormLoader } from "type-graphql-dataloader";
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  ManyToMany,
-  PrimaryGeneratedColumn,
-} from "typeorm";
-
-import Room from "./Room";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
 @Entity()
