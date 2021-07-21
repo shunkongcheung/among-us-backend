4f2302e6c2893289b85e16c913c720d7f4a284cc
shunkongcheung <shunkongcheung@gmail.com>
2021-07-21 10:54:53 -0400

    feat(PlayerResolver): remove kill and commented code

--- src/resolvers/PlayerResolver.ts
+++ src/resolvers/PlayerResolver.ts
@@ -8,7 +8,7 @@ import {
   Resolver,
 } from "type-graphql";
 
-import { ROOM_SUBSCRIPTION } from "../constants";
+import {  ROOM_SUBSCRIPTION } from "../constants";
 import { Room, Player } from "../entities";
 
 @InputType()
@@ -93,62 +93,6 @@ class PlayerResolver {
 
     return room;
   }
-
-  @Mutation(() => Room)
-  async kill(
-    @Arg("roomId") roomId: number,
-    @Arg("playerId") playerId: number,
-    @PubSub(ROOM_SUBSCRIPTION) publish: Publisher<Room>
-  ) {
-    const room = await Room.findOneOrFail({
-      where: { id: roomId },
-      relations: ["survivers", "imposters"],
-    });
-
-    const isImposter = !!room.imposters.find((itm) => itm.id === playerId);
-    if (isImposter) throw new Error("Cannot kill another imposter");
-
-    room.survivers = room.survivers.filter((itm) => itm.id !== playerId);
-
-    // crewmates# has to be more than imposters# in order to win
-    if (room.survivers.length <= room.imposters.length * 2)
-      room.endAt = new Date();
-
-    await room.save();
-
-    // trigger subscription event
-    await publish(room);
-
-    return room;
-  }
-
-  // @Mutation(() => Room)
-  // async voteOut(
-  //   @Arg("roomId") roomId: number,
-  //   @Arg("playerId") playerId: number,
-  //   @PubSub(ROOM_SUBSCRIPTION) publish: Publisher<Room>
-  // ) {
-  //   const room = await Room.findOneOrFail({
-  //     where: { id: roomId },
-  //     relations: ["survivers", "imposters"],
-  //   });
-
-  //   room.imposters = room.imposters.filter((itm) => itm.id !== playerId);
-  //   room.survivers = room.survivers.filter((itm) => itm.id !== playerId);
-
-  //   // crewmates# has to be more than imposters# in order to win
-  //   if (room.survivers.length <= room.imposters.length * 2)
-  //     room.endAt = new Date();
-
-  //   // imposters are all dead
-  //   if (!!room.imposters.length) room.endAt = new Date();
-
-  //   // trigger subscription event
-  //   await publish(room);
-
-  //   await room.save();
-  //   return room;
-  // }
 }
 
 export default PlayerResolver;
